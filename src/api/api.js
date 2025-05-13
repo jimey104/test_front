bab712e75f2c5976ce6b000926f3aa6e8c4b77de
jimey104 <jimey104@naver.com>
2025-05-13 11:05:12 +0900

    포트번호 변경

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8921',
+  baseURL: 'http://localhost:9090',
   withCredentials: true, // refreshToken이 쿠키로 올 경우 필요
 });
 
